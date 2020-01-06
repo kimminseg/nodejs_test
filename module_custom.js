ebf1cb12e681fac895baa74e3365f1f50634148d
onycom-김민석 <mins0305@onycom.com>
2020-01-06 15:41:27 +0900

    module_custom

---
+++ module_custom.js
@@ -0,0 +1,17 @@
+exports.sum = function(a,b) {
+  return a+b;
+}
+
+exports.sub = function(a,b) {
+  return a-b;
+}
+
+exports.multi = function(a,b) {
+  return a*b;
+}
+
+exports.div = function(a,b) {
+  return a/b;
+}
+
+exports.var1= 'module custom test';
---
+++ module_test.js
@@ -0,0 +1,7 @@
+var module = require('./module_custom');
+
+console.log(module.sum(10,12));
+console.log(module.sub(20,5));
+console.log(module.multi(4,21));
+console.log(module.div(20,2));
+console.log(module.var1);
